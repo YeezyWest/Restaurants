780a39ad3a26f97c75289cc1a730ffdfbb730525
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2022-09-10 21:52:41 +0100

    created a mini restaurants project

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","index.html"],
+  theme: {
+    screens: {
+      sm: '640pxpx',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    extend: {
+      colors: {
+        primary: '#ff6363',
+        secondary: {
+          100: '#e2e2d5',
+          200: '#88888e',
+        },
+       brightRed: 'hsl(12, 88%, 59%)',
+       brightRedLight: 'hsl(12, 88%, 69%)',
+       brightRedSupLight: 'hsl(12, 88%, 95%)',
+       darkBlue: 'hsl(228, 39%, 23%)',
+       darkGrayishBlue: 'hsl(227, 12%, 61%)',
+       verydarkBlue: 'hsl(233, 12%, 13%)',
+       veryPaleRed: 'hsl(13, 100%, 96%)',
+       veryLightGray: 'hsl(0, 0%, 98%)',
+      },
+    },
+    fontFamily: {
+      body: ['Nunito']
+    }
+  },
+  plugins: [],
+}
